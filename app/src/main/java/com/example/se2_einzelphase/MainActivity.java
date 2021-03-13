981e33357594e6c9005f790bb3eed3c8d5e7357f
flkrainz <flkrainz@edu.aau.at>
2021-03-13 18:10:55 +0100

    Implement sorting algorithm

--- app/src/main/java/com/example/se2_einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelphase/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,71 @@ public class MainActivity extends AppCompatActivity {
             String input = inputField.getText().toString();
 
             new WorkerThread(this, input, outputDisplay).start();
+        });
+
+        btnSort.setOnClickListener(view -> {
+            try {
+                String input = inputField.getText().toString();
+                ArrayList<Integer> evens = new ArrayList<>();
+                ArrayList<Integer> odds = new ArrayList<>();
+                int number = Integer.parseInt(input);
+                String result = "";
+
+                for (int i = 0; i < input.length(); i++) {
+                    int digit = number % 10;
+                    boolean inserted = false;
+
+                    if (digit % 2 == 0) {
+                        if (evens.size() < 1) {
+                            evens.add(digit);
+                        } else {
+                            for (int j = 0; j < evens.size(); j++) {
+                                if (digit <= evens.get(j)) {
+                                    evens.add(j, digit);
+                                    inserted = true;
+
+                                    break;
+                                }
+                            }
+
+                            if (!inserted) {
+                                evens.add(digit);
+                            }
+                        }
+                    } else {
+                        if (odds.size() < 1) {
+                            odds.add(digit);
+                        } else {
+                            for (int j = 0; j < odds.size(); j++) {
+                                if (digit <= odds.get(j)) {
+                                    odds.add(j, digit);
+                                    inserted = true;
+
+                                    break;
+                                }
+                            }
+
+                            if (!inserted) {
+                                odds.add(digit);
+                            }
+                        }
+                    }
+
+                    number = (int) (number / 10);
+                }
+
+                for (int x : evens) {
+                    result += x;
+                }
+
+                for (int x : odds) {
+                    result += x;
+                }
+
+                outputDisplay.setText(result);
+            } catch (Exception e) {
+                Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
         });
     }
 
