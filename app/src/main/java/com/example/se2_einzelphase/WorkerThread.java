1bae5c920404984d667a9524e39739045dd40fd7
flkrainz <flkrainz@edu.aau.at>
2021-03-13 17:34:53 +0100

    Implement worker thread

--- app/src/main/java/com/example/se2_einzelphase/WorkerThread.java
+++ app/src/main/java/com/example/se2_einzelphase/WorkerThread.java
@@ -2,6 +2,13 @@ package com.example.se2_einzelphase;
 
 import android.content.Context;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class WorkerThread extends Thread{
 
@@ -17,6 +24,29 @@ public class WorkerThread extends Thread{
 
     @Override
     public void run() {
+        Socket socket = null;
+        String result;
 
+        try{
+            socket = new Socket("se2-isys.aau.at", 53212);
+
+            try(DataOutputStream toServer = new DataOutputStream(socket.getOutputStream())) {
+                try (BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    byte[] data = (input + '\n').getBytes();
+
+                    toServer.write(data);
+
+                    result = fromServer.readLine();
+
+                    outputDisplay.setText(result);
+                }
+            }
+        }catch(Exception e){
+            Toast.makeText(parent, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }finally{
+            if (socket != null && !socket.isClosed()) {
+                try { socket.close(); } catch (Exception e) {}
+            }
+        }
     }
 }
