7fdab2134300fa338d84796169620c656d690d49
flkrainz <flkrainz@edu.aau.at>
2021-03-13 17:22:21 +0100

    Implement main activity logic

--- app/src/main/java/com/example/se2_einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelphase/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.se2_einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView outputDisplay;
+    private EditText inputField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        outputDisplay = findViewById(R.id.outputDisplay);
+        inputField = findViewById(R.id.inputField);
+
+        btn.setOnClickListener(view -> {
+            String input = inputField.getText().toString();
+
+            new WorkerThread(this, input, outputDisplay).start();
+        });
     }
+
+
 }
--- app/src/main/java/com/example/se2_einzelphase/WorkerThread.java
+++ app/src/main/java/com/example/se2_einzelphase/WorkerThread.java
@@ -1,5 +1,22 @@
 package com.example.se2_einzelphase;
 
-public class WorkerThread {
+import android.content.Context;
+import android.widget.TextView;
 
+public class WorkerThread extends Thread{
+
+    private Context parent;
+    private String input;
+    private TextView outputDisplay;
+
+    public WorkerThread(Context parent, String input, TextView outputDisplay){
+        this.parent = parent;
+        this.input = input;
+        this.outputDisplay = outputDisplay;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
